b6d49aea282d32609b73dd489e037f60486e39d6
Stewie Marsh <sweptsquash@live.co.uk>
2023-02-16 16:39:55 +0000

    Laravel 10 Support (#4)
    * Add Laravel 10 Support
    
    * fix: reintroduce PHP 8.1 to strategy matrices
    
    * chore: update packages and configuration

--- src/ModelHashids.php
+++ src/ModelHashids.php
@@ -15,20 +15,12 @@ class ModelHashids
     /**
      * Specify the callback that should be invoked to generate salt when no model specific generator is defined.
      * This callback takes precedence over the value specified in the config file.
-     *
-     * @param  callable|null  $callback
-     * @return void
      */
     public static function generateSaltUsing(?callable $callback): void
     {
         static::$saltGenerator = $callback;
     }
 
-    /**
-     * @param  Model  $model
-     * @param  mixed|null  $default
-     * @return mixed
-     */
     public static function generateSalt(Model $model, mixed $default = null): mixed
     {
         return (static::$saltGenerator ?? fn () => $default)($model);
@@ -37,20 +29,12 @@ class ModelHashids
     /**
      * Specify the callback that should be invoked to generate min length when no model specific generator is defined.
      * This callback takes precedence over the value specified in the config file.
-     *
-     * @param  callable|null  $callback
-     * @return void
      */
     public static function generateMinLengthUsing(?callable $callback): void
     {
         static::$minLengthGenerator = $callback;
     }
 
-    /**
-     * @param  Model  $model
-     * @param  mixed|null  $default
-     * @return mixed
-     */
     public static function generateMinLength(Model $model, mixed $default = null): mixed
     {
         return (static::$minLengthGenerator ?? fn () => $default)($model);
@@ -59,20 +43,12 @@ class ModelHashids
     /**
      * Specify the callback that should be invoked to generate an alphabet when no model specific generator is defined.
      * This callback takes precedence over the value specified in the config file.
-     *
-     * @param  callable|null  $callback
-     * @return void
      */
     public static function generateAlphabetUsing(?callable $callback): void
     {
         static::$alphabetGenerator = $callback;
     }
 
-    /**
-     * @param  Model  $model
-     * @param  mixed|null  $default
-     * @return mixed
-     */
     public static function generateAlphabet(Model $model, mixed $default = null): mixed
     {
         return (static::$alphabetGenerator ?? fn () => $default)($model);
